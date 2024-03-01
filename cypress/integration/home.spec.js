91992a684b173d0a298f933b5cb985145ed7a34b
Rose Francis <rosennabs@gmail.com>
2024-03-01 01:27:53 -0700

    Test that the products load in the homepage using cypress

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,22 @@
+describe('Jungle app', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit("http://127.0.0.1:50900");
+  })
+
+  it("should display the homepage title", () => {
+    // Use Cypress commands to interact with the page and make assertions
+    cy.get("h1").should("contain", "The Jungle");
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
